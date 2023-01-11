e272d64d56d8a551c89b2d840edc59299f244b6f
Allan Lobato <allandonizetti@gmail.com>
2023-01-11 19:24:27 -0300

    product & checkout in Stripe

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,19 +1,16 @@
-import { relative } from "path";
 import { styled } from "..";
 
 export const HomeContainer = styled("main", {
   display: "flex",
-  // gap: "3rem",
   width: "100%",
   maxWidth: "calc(100vw - ((100vw - 1180px) / 2))",
   marginLeft: "auto",
   minHeight: 656,
 });
 
-export const Product = styled("a", {
+export const Product = styled("div", {
   background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
   borderRadius: 8,
-  // padding: "0.25rem",
   cursor: "pointer",
   position: "relative",
   overflow: "hidden",
@@ -47,6 +44,7 @@ export const Product = styled("a", {
 
     strong: {
       fontSize: "$lg",
+      color: "$gray100",
     },
 
     span: {
